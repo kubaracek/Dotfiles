a7bc21efb2d055e9a51d295efc8d1f8383365be1
Jakub Racek <me@jakubracek.net>
2024-10-06 00:04:22 +0200

    Added basic neovim setup for Golang

---
+++ nvim/.config/nvim/lua/mappings.lua
@@ -0,0 +1,49 @@
+require "nvchad.mappings"
+
+-- TODO kuba. Try to figure if this M table is necessary.
+-- maybe I can use just the map fn for this?
+local M = {}
+
+M.Dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add Breakpoint at line",
+    },
+    ["<leader>dus"] = {
+      function()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sydebar with all information"
+    },
+  },
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go-to Test",
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug run last",
+    },
+  },
+}
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+return M
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,91 @@
+-- must be before lazy setup
+ vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache"
+
+ -- must be after lazy setup
+ dofile(vim.g.base46_cache .. "defaults")
+ dofile(vim.g.base46_cache .. "statusline")
+
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  "nvim-lua/plenary.nvim",
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  { -- null-ls is for formatting
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      -- require("core.utils").load_mappings("dap")
+    end
+  },
+
+  { -- Something for golang debugger
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies =  "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      -- require("core.utils").load_mappings("dap_go")
+    end
+  },
+
+  { -- What if err != nil { return err } is added automatically :brainexplosion:
+    -- TODO add :GoTagAdd json (or yaml)
+    -- and GoTestAll (generates boilerplate)
+    -- and GoIfErr (generates if err != nil ...)
+    -- as Mappings
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css"
+  		},
+  	},
+  },
+}
